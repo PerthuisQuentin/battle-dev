be38c581b002fa33bcf835cafca9c2844ad16ee7
PerthuisQuentin <perthuis.quentin@gmail.com>
2018-04-26 18:02:07 +0200

    :sparkles: [10th Edition] Add exercice 2

--- 10th-edition/README.md
+++ 10th-edition/README.md
@@ -4,7 +4,7 @@
 
 - #1 : [La bataille](exercice-1.js)
 
-- #2 : [Prix le plus bas](exercice-2.js) Soon™
+- #2 : [Prix le plus bas](exercice-2.js)
 
 - #3 : [Hydroponie](exercice-3.js) Soon™
 
--- 10th-edition/exercice-2.js
+++ 10th-edition/exercice-2.js
@@ -1,5 +1,14 @@
 const contestResponse = input => {
+	const search = input[1]
 
+	const prices = input
+		.slice(2)
+		.filter(x => x.split(' ')[0] === search)
+		.map(x => Number(x.split(' ')[1]))
+
+	const lowerPrice = prices.reduce((lower, current) => current < lower ? current : lower, prices[0])
+
+	return lowerPrice
 }
 
 module.exports = contestResponse
