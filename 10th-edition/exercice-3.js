fd4b9c0aa97296e5b78a485875149dc7f06cab5f
PerthuisQuentin <perthuis.quentin@gmail.com>
2018-04-28 21:39:55 +0200

    :sparkles: [10th Edition] Add exercice 3

--- 10th-edition/exercice-3.js
+++ 10th-edition/exercice-3.js
@@ -1,5 +1,31 @@
-const contestResponse = input => {
+const EMPTY = '.'
+const SPRINKLER = 'X'
+const WATERED = 'O'
 
+const contestResponse = input => {
+	const size = Number(input[0]) - 1
+
+	const field = input
+		.slice(1)
+		.map(x => x.split(''))
+		
+	field.forEach((row, x) => row.forEach((cell, y) => {
+			if (cell !== SPRINKLER) return
+
+			for (let a = Math.max(x - 1, 0); a <= Math.min(x + 1, size); ++a) {
+				for (let b = Math.max(y - 1, 0); b <= Math.min(y + 1, size); ++b) {
+					if (field[a][b] === EMPTY) {
+						field[a][b] = WATERED
+					}
+				}
+			}
+		}))
+
+	const wateredCellsNumber = field
+		.map(row => row.reduce((total, cell) => cell === WATERED ? total + 1 : total, 0))
+		.reduce((total, current) => current + total)
+
+	return wateredCellsNumber
 }
 
 module.exports = contestResponse
