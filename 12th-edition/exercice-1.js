d3524bf7d7c3ec6d73f6d0b2a672cf6443c91676
PerthuisQuentin <perthuis.quentin@gmail.com>
2018-11-19 10:24:32 +0100

    :sparkles: [12th Edition] Add exercice 1

--- 12th-edition/exercice-1.js
+++ 12th-edition/exercice-1.js
@@ -1,5 +1,15 @@
 const contestResponse = input => {
+	const initialPrice = Number(input[1])
 
+	const result = input	
+		.slice(2)
+		.reduce((acc, cur) => {
+			const bid = cur.split(' ')
+			const value = Number(bid[0])
+			return value > acc[0] ? [value, bid[1]] : acc
+		}, [initialPrice, 'KO'])
+	
+	return result[1].toUpperCase()
 }
 
 module.exports = contestResponse
